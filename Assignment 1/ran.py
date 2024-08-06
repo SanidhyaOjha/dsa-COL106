6eb42ee43892746504b3d5fdcfe4e105a659e7da
SanidhyaOjha <ojhasanidhya.24@gmail.com>
2024-08-06 10:38:21 +0530

    implemented stack.py

--- Assignment 1/ran.py
+++ Assignment 1/ran.py
@@ -1,10 +1,15 @@
-grid_representation = [[0,0,1], [0,0,1]]
-resu = ''
-l = len(grid_representation)
-for i in range(l):
-    for j in grid_representation[i]:
-        resu = resu +str(j)  + ' '
-    if i != l-1:
-        resu += '\n'
+from stack import *
 
-print(resu)
+stack1 = Stack()
+
+stack1.push(1)
+stack1.display()
+stack1.push(2)
+stack1.display()
+stack1.pop()
+stack1.display()
+stack1.pop()
+stack1.display()
+stack1.pop()
+stack1.display()
+
--- Assignment 1/stack.py
+++ Assignment 1/stack.py
@@ -1,5 +1,30 @@
+from exception import Empty
 class Stack:
     def __init__(self) -> None:
         #YOU CAN (AND SHOULD!) MODIFY THIS FUNCTION
-        pass
+        self._data = list()
+    def __len__(self) -> int:
+        return len(self._data)
+    def is_empty(self) -> bool:
+        if len(self._data) == 0:
+            return True
+        else:
+            return False
+    def top(self):
+        if not self.is_empty():
+            return self._data[-1]
+        else:
+            raise Empty('The stack is Empty')
+        
+    def push(self, elem):
+        self._data.append(elem)
+
+    def pop(self):
+        if not self.is_empty():
+            self._data.pop()
+        else:
+            raise Empty('The stack is Empty')
+    
+    def display(self):
+        print(self._data)
     # You can implement this class however you like
